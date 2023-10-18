99dd0bcb6bcb03149bbd3bac33c2398df0ba8524
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-18 15:36:19 +0600

    added add products functional

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -2,27 +2,27 @@ import Swal from "sweetalert2";
 
 
 const AddProduct = () => {
-    const handleAddCoffee = event => {
+    const handleAddProducts = event => {
         event.preventDefault();
         const form = event.target;
 
         const name = form.name.value;
-        const chef = form.chef.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photo = form.photo.value;
+        const brand = form.brand.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const rating = form.rating.value;
+        const image = form.image.value;
 
-        const newCoffee = {name, chef, supplier, taste, category, details, photo}
-        console.log(newCoffee);
+        const newProduct = {name, brand, type, price, description, rating, image}
+        console.log(newProduct);
 
-        fetch('http://localhost:5000/coffee', {
+        fetch('http://localhost:5000/products', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(newCoffee)
+            body: JSON.stringify(newProduct)
         })
         .then(res => res.json())
         .then(data => {
@@ -43,40 +43,40 @@ const AddProduct = () => {
     }
     return (
         <div className="max-w-6xl mx-auto bg-[#F4F3F0] p-20">
-            <h1 className="text-3xl text-center font-bold mb-4">Add New Coffee</h1>
+            <h1 className="text-3xl text-center font-bold mb-4">Add New Product</h1>
             <p className="text-center w-9/12 mx-auto">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
             <div>
-                <form onSubmit={handleAddCoffee}>
+                <form onSubmit={handleAddProducts}>
                     <div className="grid grid-cols-2 gap-5 mt-10">
                         <div>
                             <h3 className="label-text text-xl mb-3">Name</h3>
                             <input type="name" name="name" placeholder="Enter Coffee name" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div>
-                            <h3 className="label-text text-xl mb-3">Chef</h3>
-                            <input type="name" name="chef" placeholder="Enter Coffee Chef" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Brand Name</h3>
+                            <input type="name" name="brand" placeholder="Enter Brand Name" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div>
-                            <h3 className="label-text text-xl mb-3">Supplier</h3>
-                            <input type="name" name="supplier" placeholder="Enter Coffee Supplier" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Products Type</h3>
+                            <input type="name" name="type" placeholder="Enter Products Type" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div>
-                            <h3 className="label-text text-xl mb-3">Taste</h3>
-                            <input type="name" name="taste" placeholder="Enter Coffee Taste" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Price</h3>
+                            <input type="name" name="price" placeholder="Enter Products Price" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div>
-                            <h3 className="label-text text-xl mb-3">Category</h3>
-                            <input type="name" name="category" placeholder="Enter Coffee Category" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Short Description</h3>
+                            <input type="name" name="description" placeholder="Enter Short Description" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div>
-                            <h3 className="label-text text-xl mb-3">Details</h3>
-                            <input type="name" name="details" placeholder="Enter Coffee Details" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Rating</h3>
+                            <input type="name" name="rating" placeholder="Enter Products Rating" className="p-[10px] rounded-md w-full" />
                         </div>
                         <div className="col-span-2">
-                            <h3 className="label-text text-xl mb-3">Photo</h3>
-                            <input type="name" name="photo" placeholder="Enter Photo URL" className="p-[10px] rounded-md w-full" />
+                            <h3 className="label-text text-xl mb-3">Image</h3>
+                            <input type="name" name="image" placeholder="Enter Image URL" className="p-[10px] rounded-md w-full" />
                         </div>
-                       <input type="submit" className="w-full py-[10px] text-center font-medium bg-[#D2B48C] col-span-2 border text-lg rounded-md" value="Add Coffee" />
+                       <input type="submit" className="w-full py-[10px] text-center font-medium bg-[#D2B48C] col-span-2 border text-lg rounded-md" value="Add Products" />
                         
                     </div>
                 </form>
